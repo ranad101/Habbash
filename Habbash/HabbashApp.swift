267be80e260a2dd2cfe52f476452be67b1a30cac
ranad101 <aldawoodranad@outlook.com>
2025-05-24 10:26:40 +0300

    Initial Commit

---
+++ Habbash/HabbashApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbashApp.swift
+//  Habbash
+//
+//  Created by Ranad aldawood on 26/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HabbashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabbashTests/HabbashTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabbashTests.swift
+//  HabbashTests
+//
+//  Created by Ranad aldawood on 26/11/1446 AH.
+//
+
+import Testing
+@testable import Habbash
+
+struct HabbashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
