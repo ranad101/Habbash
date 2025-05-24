7e1ec3ae6f4f45aca98270a99fc9055723aec255
ranad101 <aldawoodranad@outlook.com>
2025-05-24 17:49:26 +0300

    remaining MCQs

--- Habbash/ViewModels/GameViewModel.swift
+++ Habbash/ViewModels/GameViewModel.swift
@@ -104,6 +104,132 @@ class GameViewModel: ObservableObject {
             answers: ["", "", "", ""],
             correctAnswerIndex: 0,
             questionNumber: "١٠"
+        ),
+        Question(
+            id: 11,
+            questionText: "كم عدد الموزات اللي تقدر تاكلها على معده فاضية؟",
+            answers: ["تعتمد على الحجم", "ولا وحدة", "وحدة", "خمسة"],
+            correctAnswerIndex: 2,
+            questionNumber: "١١",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 14,
+            questionText: "نتمنى انك كنت مركز لرقم السؤال",
+            answers: ["روح لسؤال ١٧", "روح لسؤال ١٥", "روح لسؤال ١٤", "روح لسؤال ١٦"],
+            correctAnswerIndex: 1,
+            questionNumber: "١٤",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 16,
+            questionText: "أيهم أثقل ؟ كيلو قطن ولا كيلو حديد",
+            answers: ["كيلو قطن", "كيلو حديد", "متساويين", "خدك"],
+            correctAnswerIndex: 2,
+            questionNumber: "١٦",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 18,
+            questionText: "اين يمكن ان تجد بيت الشعر؟",
+            answers: ["في ديون المتبني", "داخل مشط الحلاق", "في الصحراء", "فوق السطح"],
+            correctAnswerIndex: 1,
+            questionNumber: "١٨",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 20,
+            questionText: "الخيار لك فلها 😉",
+            answers: ["١+ تخطي", "تجاهل", "١+ حياة", "-١ حياة"],
+            correctAnswerIndex: 0,
+            questionNumber: "٢٠",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 23,
+            questionText: "اجابة هذا السؤال كبيرة مره",
+            answers: ["كبيرة مره", "راسي", "مرة كبيرة", "علامة لا نهايه"],
+            correctAnswerIndex: 1,
+            questionNumber: "٢٣",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 29,
+            questionText: "وش لون جناح الطيارة؟",
+            answers: ["برتقالي", "ماله لون معين", "بخير", "ابيض"],
+            correctAnswerIndex: 0,
+            questionNumber: "٢٩",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 32,
+            questionText: "هيبه وتأمر على عشرين رجال وحرمه ؟",
+            answers: ["الصافرة", "الحكم", "المعلم", "الجرس"],
+            correctAnswerIndex: 0,
+            questionNumber: "٣٢",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 33,
+            questionText: "وش تاسع حرف من الحروف الأبجدية",
+            answers: ["هـ", "ذ", "ر", "كأن هذا السوال مر علي🤔"],
+            correctAnswerIndex: 1,
+            questionNumber: "٣٣",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 35,
+            questionText: "لو حطيت طماطم في البحر وش بيصير لها ؟",
+            answers: ["تروح رحله بحريه", "تغرق", "تصير عصير", "ياكلونها سمك"],
+            correctAnswerIndex: 0,
+            questionNumber: "٣٥",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 39,
+            questionText: "متى يذوب الحديد",
+            answers: ["اذا خاف", "اذا انصهر", "اذا اشتاق", "اذا شافك"],
+            correctAnswerIndex: 1,
+            questionNumber: "٣٩",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 44,
+            questionText: "وش من الخيارات موجود تحت العين",
+            answers: ["ت", "رموش", "مسافة", "خشم"],
+            correctAnswerIndex: 0,
+            questionNumber: "٤٤",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 46,
+            questionText: "وش يصير إذا انقطع النت",
+            answers: ["تنام بدري", "العالم ينهار", "يزعل الرواتر", "مافي تواصل"],
+            correctAnswerIndex: 0,
+            questionNumber: "٤٦",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
+        ),
+        Question(
+            id: 47,
+            questionText: "انتبه ترا اذا قلته بينكسر !",
+            answers: ["المواعين", "الصمت", "المراية", "رقبة الثور"],
+            correctAnswerIndex: 1,
+            questionNumber: "٤٧",
+            questionFontSize: 28,
+            answerFontSizes: [22, 20, 18, 24]
         )
     ]
 
